cb2d4a24f303ec8ff373a4237de621389b846a58
Emupedia <admin@emupedia.net>
2020-03-22 01:43:46 +0200

    More work

--- js/main.js
+++ js/main.js
@@ -485,13 +485,33 @@
 					if (net.client_cmd(data)) {
 						// noinspection JSUnresolvedFunction
 						net.text_input.val('');
-
 						return true;
 					}
 
 					// noinspection JSUnresolvedFunction
-					if (data.cmd === 'nick' && data.data === '') {
-						return false;
+					if (data.cmd === 'nick') {
+						if (data.data === '') {
+							setTimeout(function () {
+								alert('Nickname cannot be empty!');
+							}, 100);
+							return false;
+						}
+
+						if (~data.data.toLowerCase().indexOf('admin') || ~data.data.toLowerCase().indexOf('emupedia') || ~data.data.toLowerCase().indexOf('emuos')) {
+							setTimeout(function () {
+								alert('Nickname not allowed!');
+							}, 100);
+							return false;
+						}
+
+						for (var user in net.room_info.users) {
+							if (net.room_info.users[user].info.nick.toLowerCase() === data.data) {
+								setTimeout(function () {
+									alert('Nickname already taken!');
+								}, 100);
+								return false;
+							}
+						}
 					}
 
 					// noinspection JSUnresolvedFunction
