c0f07664eb9544e23cd0b0d21f8d2b08e866a578
Emupedia <admin@emupedia.net>
2021-05-20 05:53:45 +0300

    More work

--- js/main.js
+++ js/main.js
@@ -956,10 +956,10 @@
 					// noinspection JSUnresolvedFunction
 					if (data.cmd === 'nick') {
 						data.data = net.remove_spam(net.remove_duplicates(net.remove_numbers(net.remove_zalgo(net.normalize(data.data, normalize_types)))));
-						data.data = data.data.replace(/[.,'"]/g, '');
+						data.data = data.data.replace(/[`.,'"]/g, '');
 
-						if ((net.remove_combining(net.remove_invisible(data.data))).trim() === '' || (net.remove_combining(net.remove_invisible(data.data))).trim().length <= 1) {
-							net.log('You have unwanted characters in your nickname or it is too short, correct the issue and try again', 4);
+						if (!/[a-z]/i.test(data.data) || (net.remove_combining(net.remove_invisible(data.data))).trim() === '' || (net.remove_combining(net.remove_invisible(data.data))).trim().length <= 1) {
+							net.log('You have unwanted characters in your nickname or it is too short, correct the issue and try again.', 4);
 							return false;
 						}
 					}
