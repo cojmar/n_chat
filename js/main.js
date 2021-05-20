91b6969d05b77db08bbdecca73f108a22d8c263b
Emupedia <admin@emupedia.net>
2021-05-20 06:04:23 +0300

    More work

--- js/main.js
+++ js/main.js
@@ -962,7 +962,7 @@
 						data.data = net.remove_spam(net.remove_duplicates(net.remove_numbers(net.remove_zalgo(net.normalize(data.data, normalize_types)))));
 						data.data = data.data.replace(/[`.,'"]/g, '');
 
-						if (!/[a-z]/i.test(net.normalize(data.data)) || (net.remove_combining(net.remove_invisible(data.data))).trim() === '' || (net.remove_combining(net.remove_invisible(data.data))).trim().length <= 1) {
+						if (!/[a-z]/i.test(net.normalize(data.data)) || (net.remove_combining(net.remove_invisible(data.data))).trim() === '' || (net.remove_combining(net.remove_invisible(data.data))).trim().length <= 2) {
 							net.log('You have unwanted characters in your nickname or it is too short, correct the issue and try again.', 4);
 							return false;
 						}
