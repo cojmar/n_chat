e4dd575debf853eb890c00e730bf08f0fb9c3fdc
Emupedia <admin@emupedia.net>
2021-05-31 09:29:54 +0300

    More work

--- js/main.js
+++ js/main.js
@@ -473,7 +473,7 @@
 			};
 
 			net.remove_combining = function(str) {
-				return str.replace(/[\u0336\u0337\u0489\u064d\u0650\u065e\u20d8\ufc5e]/g, '');
+				return str.replace(/[\u0336\u0337\u0489\u064d\u0650\u065c\u065e\u20d8\ufc5e]/g, '');
 			};
 
 			net.remove_numbers = function(str) {
