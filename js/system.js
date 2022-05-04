bc1aa68776e094247ad92b133a6e15a64284ecf1
Emupedia <admin@emupedia.net>
2022-05-04 21:57:04 +0300

    More work

--- js/system.js
+++ js/system.js
@@ -15,7 +15,8 @@
 		browser: {},
 		feature: {},
 		lib: global.$sys ? global.$sys.lib || {} : {},
-		info: {}
+		info: {},
+		version: '{{ site.github.build_revision }}'
 	};
 
 	// region Platform
