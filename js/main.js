478b3f8597c006338124903de662caed55fd7f64
Emupedia <admin@emupedia.net>
2022-06-05 06:09:06 +0300

    More work

--- js/main.js
+++ js/main.js
@@ -923,11 +923,13 @@
 					var found = false;
 
 					for (var event in eventss) {
+						var extension = 'gif';
 						var count = 0;
 						var words = [];
 
 						if (typeof events.mapping[event_language][eventss[event]] === 'object' && !Array.isArray(events.mapping[event_language][eventss[event]]) && events.mapping[event_language][eventss[event]] !== null) {
-							count = events.mapping[event_language][eventss[event]].count;
+							extension = events.mapping[event_language][eventss[event]]['type'] || extension;
+							count = events.mapping[event_language][eventss[event]]['count'];
 							// noinspection JSUnresolvedVariable
 							words = events.mapping[event_language][eventss[event]].triggers.sort(function(a, b) {
 								return b.length - a.length;
@@ -941,7 +943,7 @@
 						for (var word in words) {
 							// noinspection JSUnfilteredForInLoop
 							if (~str.toLowerCase().indexOf(words[word])) {
-								net.render_event(eventss[event], count > 0 ? (Math.floor(Math.random() * (count - 1 + 1) + 1)) : count);
+								net.render_event(eventss[event], count > 0 ? net.random_integer(Math.random(), 1, count) : count, extension);
 								found = true;
 								break;
 							}
@@ -1155,10 +1157,10 @@
 				}
 			};
 
-			net.render_event = function(type, count) {
+			net.render_event = function(type, count, extension) {
 				if (net.event.find('audio, video, iframe').length === 0) {
 					// noinspection HtmlUnknownTarget
-					net.event.html('<div class="animate__animated animate__zoomIn"><span>&nbsp;</span><img src="images/events/' + type + (count > 0 ? count : '') + '.gif" alt="" /></div>');
+					net.event.html('<div class="animate__animated animate__zoomIn"><span>&nbsp;</span><img src="images/events/' + type + (count > 0 ? count : '') + '.' + extension + '" alt="" /></div>');
 
 					clearTimeout(net.event_timeout);
 					net.event_timeout = setTimeout(function() {
